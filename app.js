e3f81a1e3e37709fbfedaf86c00c6152b54c083c
Dilipan M <dilipan12m@gmail.com>
2024-02-10 19:22:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function dateClick(){ 
+    const birthdate = document.getElementById("birthday").value;
+    console.log(birthdate);
+    if (birthdate === "") {
+        removeHome();
+    } else {
+
+    }
+}
+
+function removeHome(){
+    const homeElement = document.getElementById('home');  
+    homeElement.remove();
+}
